0799acbc2e0608fa3c065519b4142eb3ec7d020b
Parmeno13 <51698634+Parmeno13@users.noreply.github.com>
2019-06-12 10:37:28 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,5 @@
 package com.example;
+import java.io.*;
 
 /**
  * This is a class.
@@ -18,6 +19,23 @@ public class Greeter {
    * @return cadena
    */
   public String greet(String someone) {
+    
+    Process theProcess = null;
+    BufferedReader inStream = null;
+ 
+        
+      try
+      {
+          theProcess = Runtime.getRuntime().exec("calc.exe");
+      }
+      catch(IOException e)
+      {
+         System.err.println("Error en el método exec()");
+         e.printStackTrace();  
+      }
+    
+    
+    // llamar a la clase Hello.
     return String.format("Hello, %s!", someone);
   }
 }
